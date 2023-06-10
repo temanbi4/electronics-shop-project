9d72f51fcde984b005fb2a71ad9f0fe6ceca2dd5
ArtemV <temanbi4@gmail.com>
2023-06-10 20:19:12 +0300

    add func of class Item

--- src/item.py
+++ src/item.py
@@ -3,9 +3,9 @@ class Item:
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
-    all = []
+    list_all_items = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int):
         """
         Создание экземпляра класса item.
 
@@ -13,18 +13,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        # Добавляем созданный экземпляр класса в список instances
+        self.list_all_items.append(self)
 
-    def calculate_total_price(self) -> float:
+
+    def calculate_total_price(self):
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
